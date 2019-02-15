b743845ac110b4a63fb924a3ff8b481686cf354d
adflx <ian.felix18@gmail.com>
2019-02-15 23:40:29 +0800

    added scripts and scene for character selection

---
+++ Space Buster/Assets/CharacterManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterManager : MonoBehaviour {
+
+    public CharacterInfo[] Characters;
+
+    public GameObject SpawnPoint;
+
+    private int _currentIndex = 0;
+
+    private CharacterInfo _currentCharacterType = null;
+
+    private CharacterInfo _currentCharacter = null;
+
+ 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
