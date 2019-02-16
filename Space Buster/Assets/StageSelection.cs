98a0a88c42a466f3a7d14d6244d8bb6c06892761
adflx <ian.felix18@gmail.com>
2019-02-16 16:16:13 +0800

    Done Stage Scene Selection

---
+++ Space Buster/Assets/StageSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class StageSelection : MonoBehaviour
+{
+
+    public void FallenEarth()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+    public void BusterBase()
+    {
+        SceneManager.LoadScene("Stage2");
+    }
+
+    public void StigmaBase()
+    {
+        SceneManager.LoadScene("Stage3");
+    }
+
+}
